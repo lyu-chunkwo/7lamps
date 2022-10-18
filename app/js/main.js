9212ce813cf7ab9f53c9a3f14de7f42d91612366
Andrew <lyu.chun.kwo@gmail.com>
2022-10-18 10:33:43 +0600

    finish project

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,26 @@
 $(function () {
 
 
+  $('.product-single__slider').slick({
+    asNavFor: '.product-single__slider-sync',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: false,
+    arrows: false,
+    fade: true
+  });
+
+  $('.product-single__slider-sync').slick({
+    asNavFor: '.product-single__slider',
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    arrows: false,
+    variableWidth: true,
+    dots: false
+  });
+
+
   const wow = new WOW(
     {
       boxClass: 'wow',
@@ -67,6 +87,9 @@ $(function () {
       $('.checkout__payment-hiddentext1').slideToggle(300);
       $('.checkout__payment-hiddentext2').slideUp(300);
       $('.checkout__payment-hiddentext3').slideUp(300);
+      $('.checkout__payment-radiostyle').addClass('radio-active');
+      $('.checkout__payment-radiostyle2').removeClass('radio-active');
+      $('.checkout__payment-radiostyle3').removeClass('radio-active');
     }
   });
   $(".checkout__payment-radio2").on("change", function () {
@@ -74,6 +97,9 @@ $(function () {
       $('.checkout__payment-hiddentext2').slideToggle(300);
       $('.checkout__payment-hiddentext1').slideUp(300);
       $('.checkout__payment-hiddentext3').slideUp(300);
+      $('.checkout__payment-radiostyle2').addClass('radio-active');
+      $('.checkout__payment-radiostyle').removeClass('radio-active');
+      $('.checkout__payment-radiostyle3').removeClass('radio-active');
     }
   });
   $(".checkout__payment-radio3").on("change", function () {
@@ -81,6 +107,9 @@ $(function () {
       $('.checkout__payment-hiddentext3').slideToggle(300);
       $('.checkout__payment-hiddentext2').slideUp(300);
       $('.checkout__payment-hiddentext1').slideUp(300);
+      $('.checkout__payment-radiostyle3').addClass('radio-active');
+      $('.checkout__payment-radiostyle').removeClass('radio-active');
+      $('.checkout__payment-radiostyle2').removeClass('radio-active');
     }
   });
 
@@ -133,5 +162,57 @@ $(function () {
 
   let inputValue = $('.cart__table-item--price').data('price');
   console.log(inputValue);
+
+
+  $('#check').change(function () {
+    if ($(this).is(":checked")) {
+      $('.checkout__checkbox-style').addClass("check-active");
+    } else {
+      $('.checkout__checkbox-style').removeClass("check-active");
+    }
+  });
+
+  $('#check-terms').change(function () {
+    if ($(this).is(":checked")) {
+      $('.checkout__payment-checkboxstyle').addClass("check-active");
+    } else {
+      $('.checkout__payment-checkboxstyle').removeClass("check-active");
+    }
+  });
+
+  $('#check-popup').change(function () {
+    if ($(this).is(":checked")) {
+      $('.popup__check-style').addClass("popup-active");
+    } else {
+      $('.popup__check-style').removeClass("popup-active");
+    }
+  });
+
+
+
+  $(".product-single__accordion-content").css("display", "none");
+  $(".product-single__name").on("click", function () {
+    $(".product-single__name").not(this).removeClass("open");
+    $(".product-single__name").not(this).next().slideUp(300);
+    $(this).toggleClass("open");
+    $(this).next().slideToggle(300);
+  });
+
+
+
+  $('.product-single__tabs-btn').on('click', function (e) {
+    e.preventDefault();
+    $('.product-single__tabs-btn').removeClass('active');
+    $(this).addClass('active');
+
+    $('.product-single__tab').removeClass('active');
+    $($(this).attr('href')).addClass('active');
+  });
+
+  $('.product-single__tabs-button').on('click', function (e) {
+    e.preventDefault();
+    $('.product-single__tabs-button').removeClass('active');
+    $(this).addClass('active');
+  });
 
 });
